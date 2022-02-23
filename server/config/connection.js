33f8e2b87a85f5a0d14ecff9c7bd8c3c65016528
danny p <danny@dannys-MacBook-Pro.local>
2022-02-23 14:41:31 -0500

    pre deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/locations', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/go-here-db', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
